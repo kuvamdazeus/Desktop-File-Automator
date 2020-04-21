94dc7e2ea331291562acbb150cf0b190d74b2593
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2020-04-21 16:18:25 +0530

    fixed minor but achy bugs

--- distributor.py
+++ distributor.py
@@ -1,32 +1,39 @@
 #! /usr/bin/env python3
-import os, re
+import re, os
 def process_destination():
     file = open("file_addresses.txt")
-    place_pattern = r"\s?:\s?([a-zA-Z/_])"
+    place_pattern = r"\s?:\s?([a-zA-Z/_~]+)"
+    string_places = []
     places = []
     for line in file:
         place = re.search(place_pattern, line)
-        places.append(place)
+        places.append(place.group(1))
     file.close()
+    print(places)
     return places
 
 def move(places):
     text, python, bash, image = places
-    text = text.group()
-    python = python.group()
-    image = image.group()
+    print(text, python, bash, image)
     # check that the pattern is right or not
     # Try from here :)
     # search for "how to find the type of file in python3"
-    for file in os.listdir():
+    os.chdir("./Distributor")
+    files = os.listdir()
+    os.chdir("..")
+    print(files)
+    for file in files:
+        print(file)
+        if file == "distributor.py" or file == "file_addresses.txt" or file == "setup.sh":
+            continue
         if ".txt" in file and os.path.exists(text):
-            os.system("mv {} {}".format(file, text))
+            os.system("mv ./Distributor/{} {}".format(file, text))
         elif ".py" in file and os.path.exists(python):
-            os.system("mv {} {}".format(file, python))
+            os.system("mv ./Distributor/{} {}".format(file, python))
         elif ".sh" in file and os.path.exists(bash):
-            os.system("mv {} {}".format(file, bash))
+            os.system("mv ./Distributor/{} {}".format(file, bash))
         elif (".jpg" in file or "png" in file or ".jpeg" in file) and os.path.exists(image):
-            os.system("mv {} {}".format(file, image))
+            os.system("mv ./Distributor/{} {}".format(file, image))
 
 def main():
     move(process_destination())
