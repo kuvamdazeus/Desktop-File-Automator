a95334b234bbbbd0e518963ce88b126a1679ba52
kuvamdazeus <kuvamyash@gmail.com>
2020-04-23 20:48:19 +0530

    prepared the file to run infinitely but is not tested, testing to be done

--- distributor.py
+++ distributor.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python3
 import re, os, sys
 def process_destination():
-    file = open("file_addresses.txt")
+    try:
+      file = open("file_addresses.txt")
+    except Exception:
+      print("File file_addresses.txt was not found, make sure it is in the same directory as the distributor.py is")
+      sys.exit(1)
     place_pattern = r"\s?:\s?([a-zA-Z0-9\-/_]+)" # pattern to extract the file name from file_addresses.txt
     places = []
     for line in file:
         place = re.search(place_pattern, line)
         if place != None:
             places.append(place.group(1)) # extract and append in places list
-    print(places)
     file.close()
-    print(places)
     return places
 
 def move(places): # places is the list of destinations provided in the file_addresses.txt
@@ -23,28 +25,35 @@ def move(places): # places is the list of destinations provided in the file_addr
     # Try from here :)
     os.chdir("./Distributor")
     files = os.listdir()
+    if len(files) is 0:
+      print("Searching...")
     os.chdir("..")
     for file in files: # Extensions are most important, file which do not has extension will be left in distributor
         if file == "distributor.py" or file == "file_addresses.txt" or file == "setup.sh":
             continue
         if ".txt" in file and os.path.exists(text): # check the extension of file and then check if the given destination exists or not
             os.system("mv ./Distributor/{} {}".format(file, text))
+            print("{} was transferred".format(file))
         elif ".py" in file and os.path.exists(python):
             os.system("mv ./Distributor/{} {}".format(file, python))
+            print("{} was transferred".format(file))
         elif ".sh" in file and os.path.exists(bash):
             os.system("mv ./Distributor/{} {}".format(file, bash))
+            print("{} was transferred".format(file))
         elif (".jpg" in file or "png" in file or ".jpeg" in file) and os.path.exists(image):
             os.system("mv ./Distributor/{} {}".format(file, image))
+            print("{} was transferred".format(file))
     os.chdir("./Distributor")
     if len(os.listdir()) is 0:
         print("{} Files transfered successfully".format(len(files)))
+        sys.exit(0)
     else:
         print("Remaining files: ")
         count = 0
         for file in os.listdir():
             count += 1
-            print("{}. {}".format(count, file))
-    os.chdir("..")
+            print("{}. {}".format(file))
+        sys.exit(0)
 
 def main():
     move(process_destination())
